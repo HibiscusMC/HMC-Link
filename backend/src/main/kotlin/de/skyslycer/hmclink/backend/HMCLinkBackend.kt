7a9d1ef06d14d8672d252b7d379b3c9c57a810ea
Skyslycer <skyslycer@gmx.net>
2022-01-05 23:47:32 +0100

    fix redis broken check and maybe fix docker
    Took 12 minutes

--- backend/src/main/kotlin/de/skyslycer/hmclink/backend/HMCLinkBackend.kt
+++ backend/src/main/kotlin/de/skyslycer/hmclink/backend/HMCLinkBackend.kt
@@ -25,7 +25,7 @@ class HMCLinkBackend {
         System.getenv(EnvironmentVariables.REDIS_PORT).toInt()
     )
 
-    private val distributor = MessageDistributor(messageHandler)
+    private lateinit var distributor: MessageDistributor
 
     /**
      * A basic method to start the backend.
@@ -34,7 +34,6 @@ class HMCLinkBackend {
         logger.info("Starting the HMC Link Backend!")
 
         logger.info("Initializing Redis...")
-
         val redisReturn = messageHandler.createJedis()
 
         if (redisReturn.isPresent) {
@@ -42,17 +41,16 @@ class HMCLinkBackend {
             exitProcess(1)
         }
 
+        distributor = MessageDistributor(messageHandler)
         logger.info("Successfully connected to Redis!")
 
         logger.info("Initializing database...")
-
         val databaseReturn = DatabaseHandler.connect()
 
         if (databaseReturn.isPresent) {
             logger.error("Couldn't enable the database! Shutting down the backend.", redisReturn.get())
             exitProcess(1)
         }
-
         logger.info("Successfully connected to the database!")
 
         logger.info("Setting up message listeners...")
